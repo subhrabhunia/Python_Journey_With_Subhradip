e58c98a13b9a121410d5e65a7509f708c10fc063
Subhradip Bhunia <smya9971@gmail.com>
2026-08-10 15:26:43 +0530

    add file

--- Introduction_To_Python/PYTHON/1st-program.py
+++ Introduction_To_Python/PYTHON/1st-program.py
@@ -1,13 +1,20 @@
 #how to print a string in Python.
 print("Hello World!") #Double quotes are used to define a string in Python.
+
 print('Hello World!') #Single quotes are also used to define a string in Python.    
+
 print("Hello" , "World!") #The above line prints two strings "Hello" and "World!" separated by a space. The comma (,) is used to separate multiple items in the print function, and it automatically adds a space between them when printed.
 
 print("Hello" + "World!") #The above line concatenates two strings "Hello" and "World!" without any space in between. The plus (+) operator is used to concatenate strings in Python.   
+
 print("Hello" + " " + "World!") #The above line concatenates two strings "Hello" and "World!" with a space in between. The plus (+) operator is used to concatenate strings in Python, and we can add a space by including it as a string in the concatenation. 
+
 print("Hello" * 3) #The above line prints the string "Hello" three times. The asterisk (*) operator is used to repeat a string in Python.   
+
 print("Hello" * 3 + "World!") #The above line prints the string "Hello" three times followed by the string "World!". The asterisk (*) operator is used to repeat a string in Python, and the plus (+) operator is used to concatenate strings.  
+
 print(50-40) #The above line prints the result of the subtraction operation, which is 10.
+
 print("sum of 50 and 40 is", 50+40) #The above line prints the string "sum of 50 and 40 is" followed by the result of the addition operation, which is 90. The comma (,) is used to separate multiple items in the print function, and it automatically adds a space between them when printed. 
 
 
