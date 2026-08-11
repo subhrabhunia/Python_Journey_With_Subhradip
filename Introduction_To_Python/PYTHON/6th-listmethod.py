3d894c34098353842702850801da5c879563c748
Subhradip Bhunia <smya9971@gmail.com>
2026-08-11 23:37:58 +0530

    add file

---
+++ Introduction_To_Python/PYTHON/6th-listmethod.py
@@ -0,0 +1,19 @@
+"""Different Types Of List Method"""
+# 1.insert()
+# 2.append()
+# 3.count()
+# 4.clear()
+# 5.copy()
+# 6.extend()
+# 7.index()
+# 8.pop()
+# 9.remove()
+# 10.reverse()
+# 11.sort()
+
+"""Different Types Of Universal List Method"""
+# 1.len(list)
+# 2.max(list)
+# 3.min(list)
+# 4.list(seq)
+
