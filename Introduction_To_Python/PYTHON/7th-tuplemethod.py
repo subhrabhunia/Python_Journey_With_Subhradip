ba627cd8f0694bc0ddbb228bd0851dcebe02bd03
Subhradip Bhunia <smya9971@gmail.com>
2026-08-12 22:49:48 +0530

    add new file

---
+++ Introduction_To_Python/PYTHON/7th-tuplemethod.py
@@ -0,0 +1,16 @@
+# We Know that we cannot modify tuple
+"""In Build Method"""
+#1.count()
+#2.index()
+"""In Build Function"""
+# 1.len(tuple)
+# 2.max(tuple)
+# 3.min(tuple)
+# 4.tuple(iterable)
+
+fruits=("apple","banana","grapes","apple","Orange","apple")
+#fruits="apple","banana","grapes","apple","Orange","apple" This is also a rule to write the tuple.Implicit Tuples
+#print(fruits) 
+#Nested_tuple=((10,20),(30,40),(50,60))
+#print(Nested_tuple)
+print(fruits.count("apple")) #In count method its count a item how many time exits in the list.
