9947e509c1990c60e3867281e442955c3652c2b0
Subhradip Bhunia <smya9971@gmail.com>
2026-08-13 21:31:49 +0530

    add new file

--- Introduction_To_Python/PYTHON/8th-dictionary.py
+++ Introduction_To_Python/PYTHON/8th-dictionary.py
@@ -8,15 +8,19 @@
 student={ 
     "name":"John",
     "age":20,
-    "city":"Kolkata"
+    "city":"Kolkata",
+    "marks":89.90,
+    "passed":True,
+    "subject":["python","Javascript","Html"]  #In dictionary we can add multiple data tpes like string,list,etc.
     }
 
 student["gender"]="male" #If we want to any new key value in the dictionary.
 
-student["age"]=21 #If we want to change any key name or value.
+student["age"]=21 #If we want to change/update any key name or value.
 
 print(student) #This print all values of dictionary.
 print(type(student)) #This print all the values.  
 print(student["name"]) #if we want to see any one key value.This print name.  
 print(student["age"]) #This print age.
-print(student["city"]) #This print city.
+print(student["city"]) #This print city.   
+print(student["subject"][1]) #If we want to print any value under specific value in this print under subject index 1 value.
