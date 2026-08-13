420383e9e6cc5b291dc5fc28d66e4babcff1e16b
Subhradip Bhunia <smya9971@gmail.com>
2026-08-13 21:11:39 +0530

    add new file

---
+++ Introduction_To_Python/PYTHON/8th-dictionary.py
@@ -0,0 +1,6 @@
+# KEY POINTS OF DICTIONARY:
+# 1.Keys must be unique.
+# 2.Keys can be string,numbers,tuples,etc.
+# 3.Values can be of any data type.
+# 4.Dictionaries are mutable(changeable)
+# 5.Access values using keys.
