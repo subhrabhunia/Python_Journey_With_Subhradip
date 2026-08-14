db6ccf6b8ecba6f6f67d89f28a7f47cd9ded4cb3
Subhradip Bhunia <smya9971@gmail.com>
2026-08-14 22:24:29 +0530

    add new1 file

---
+++ Introduction_To_Python/PYTHON/10th-set.py
@@ -0,0 +1,6 @@
+"""KEY POINTS OF SET"""
+# 1.stores unique values only.
+# 2.duplicate values are automatically removed.
+# 3.Unordered(no indexing)
+# 4.Mutable(can add/remove items).
+# 5.Can contain different data types.
