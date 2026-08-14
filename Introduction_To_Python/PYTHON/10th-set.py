867e21d8cdf8a4e876c51e184028191835675acd
Subhradip Bhunia <smya9971@gmail.com>
2026-08-14 22:33:31 +0530

    add new file

--- Introduction_To_Python/PYTHON/10th-set.py
+++ Introduction_To_Python/PYTHON/10th-set.py
@@ -4,3 +4,12 @@
 # 3.Unordered(no indexing)
 # 4.Mutable(can add/remove items).
 # 5.Can contain different data types.
+
+"""SET IN BUILD METHOD"""
+# 1.add()
+# 2.update()
+# 3.remove()
+# 4.discard()
+# 6.pop()
+# 7.clear()
+# 8.copy()
