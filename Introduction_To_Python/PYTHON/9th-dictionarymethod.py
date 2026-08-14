83b1785cb06571278daa0da789f0a32a662339e2
Subhradip Bhunia <smya9971@gmail.com>
2026-08-14 21:04:13 +0530

    add new file

---
+++ Introduction_To_Python/PYTHON/9th-dictionarymethod.py
@@ -0,0 +1,16 @@
+"""DICTIONARYN METHODS"""
+# 1.get()
+# 2.values()
+# 3.keys()
+# 4.items()
+# 5.fromkeys()
+# 6.pop()
+# 7.popitem()
+# 8.clear()
+# 9.update()
+# 10.copy()
+# 11.setdefault()
+
+"""IN BUILD METHOD"""
+# 1.len(dict)
+# 2.str(dict)
