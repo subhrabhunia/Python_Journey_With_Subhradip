321e82e7059fc8b2f59a4d8a100ca953ddec26b8
Subhradip Bhunia <smya9971@gmail.com>
2026-08-14 22:41:26 +0530

    add new file

---
+++ Introduction_To_Python/PYTHON/11th-setmethod.py
@@ -0,0 +1,12 @@
+"""SET OPERATIONS METHODS"""
+# 1.union()
+# 2.intersection()
+# 3.differnce_update()
+# 4.difference()
+# 5.symmetric_difference()
+
+"""SET COMPARISON METHODS"""
+# 1.issubset()
+# 2.issuperset()
+# 3.isdisjoint()
+# 4.intersection_update()
