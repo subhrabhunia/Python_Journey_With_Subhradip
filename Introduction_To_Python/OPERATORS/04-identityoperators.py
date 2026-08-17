4c95ce3c3b63bade8c540c5518fdebc355b3d5fe
Subhradip Bhunia <smya9971@gmail.com>
2026-08-17 23:12:14 +0530

    add new file

--- Introduction_To_Python/OPERATORS/04-identityoperators.py
+++ Introduction_To_Python/OPERATORS/04-identityoperators.py
@@ -2,3 +2,22 @@
 # | -------- | ------------------------------------------------------------ | ------------ | ----------------- |
 # | `is`     | Returns `True` if both variables point to the same object.   | `a is b`     | `True` or `False` |
 # | `is not` | Returns `True` if both variables point to different objects. | `a is not b` | `True` or `False` |
+
+
+x=[10,20,30]
+z=x
+y=[10,20,30]
+
+print(x is y)  #Its print false because its two values are same but two varriables store in different different memory locations.It is for list dictionar set tuple.
+print(x is z)
+
+a=100
+b=100
+print(a is b)  #if two varriables has single values then its stores same memory location so its give true.
+
+
+c=[10,20,30]
+d=c  
+e=[10,20,30]
+print(c is not e) #Its print True because two varriables stores diferent memory.
+print (c is not d) #Its print False beacuse two varriables store in same memory.
