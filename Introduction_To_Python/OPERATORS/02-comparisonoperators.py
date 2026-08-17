e7af1046c8ba808dc32bf9aed09582424c3829be
Subhradip Bhunia <smya9971@gmail.com>
2026-08-17 22:19:47 +0530

    add new file

--- Introduction_To_Python/OPERATORS/02-comparisonoperators.py
+++ Introduction_To_Python/OPERATORS/02-comparisonoperators.py
@@ -8,3 +8,20 @@
 # | `<=`     | Less than or equal to    | `5 <= 10`  | `True` |
 
 
+a=10
+b=20
+print(a==b)   #its check two variable is equal or not.if equal give True or not equal then false.if two varriable  value same but one varriable integer and one varriable string then its print False.Its check all tuples list.
+
+print(a!=b)   #Its check two varriables is not eqal. if not equal then give output True.
+
+print(a>b)  #Its check first varriables is large than the second varriables.
+
+print(a>=b)   #Its check two varriables is same or first varriables is largest than second varriables.
+
+print(a<b)  #Its check first varriables is smaller than secon varriable.
+
+print(a<=b)  #Its check first varriable is smaller than second varriable  or two varriables equal.
+
+
+age=20
+print(age>=18)
