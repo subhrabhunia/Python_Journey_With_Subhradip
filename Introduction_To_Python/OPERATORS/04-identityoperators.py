b419bbf21ed6152f34aee20769984083a2f1de3b
Subhradip Bhunia <smya9971@gmail.com>
2026-08-17 22:58:49 +0530

    add new file

---
+++ Introduction_To_Python/OPERATORS/04-identityoperators.py
@@ -0,0 +1,4 @@
+# | Operator | Description                                                  | Example      | Result            |
+# | -------- | ------------------------------------------------------------ | ------------ | ----------------- |
+# | `is`     | Returns `True` if both variables point to the same object.   | `a is b`     | `True` or `False` |
+# | `is not` | Returns `True` if both variables point to different objects. | `a is not b` | `True` or `False` |
