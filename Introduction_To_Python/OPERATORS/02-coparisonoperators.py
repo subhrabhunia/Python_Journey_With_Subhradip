bab3df11651f42b23b12664ef224e30f9be8fb41
Subhradip Bhunia <smya9971@gmail.com>
2026-08-17 21:58:38 +0530

    add new file

---
+++ Introduction_To_Python/OPERATORS/02-coparisonoperators.py
@@ -0,0 +1,10 @@
+# | Operator | Description              | Example    | Result |
+# | -------- | ------------------------ | ---------- | ------ |
+# | `==`     | Equal to                 | `5 == 5`   | `True` |
+# | `!=`     | Not equal to             | `5 != 3`   | `True` |
+# | `>`      | Greater than             | `10 > 5`   | `True` |
+# | `<`      | Less than                | `5 < 10`   | `True` |
+# | `>=`     | Greater than or equal to | `10 >= 10` | `True` |
+# | `<=`     | Less than or equal to    | `5 <= 10`  | `True` |
+
+
