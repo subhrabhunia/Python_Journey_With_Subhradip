eb121a4596adff4f15b114da15156889886d1bda
Subhradip Bhunia <smya9971@gmail.com>
2026-08-17 22:33:58 +0530

    add new file

---
+++ Introduction_To_Python/OPERATORS/03-logicaloperators.py
@@ -0,0 +1,8 @@
+# In logical operators check many condition in same time.Using many comparison creates a logical operators.
+
+# | Operator | Description                                                   | Example                | Result  |
+# | -------- | ------------------------------------------------------------- | ---------------------- | ------- |
+# | `and`    | Returns `True` if both conditions are `True`.                 | `(5 > 2) and (10 > 5)` | `True`  |
+# | `or`     | Returns `True` if at least one condition is `True`.           | `(5 > 10) or (10 > 5)` | `True`  |
+# | `not`    | Reverses the result. Returns `False` if the result is `True`. | `not(5 > 2)`           | `False` |
+
