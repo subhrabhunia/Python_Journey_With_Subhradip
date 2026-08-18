baae30639c822ddb0eee08b97b9f736e9132436e
Subhradip Bhunia <smya9971@gmail.com>
2026-08-18 21:39:06 +0530

    add new file

--- Introduction_To_Python/OPERATORS/OPERATOR_FOLDER_PRACTICE_QUESTION/Question_part2.py
+++ Introduction_To_Python/OPERATORS/OPERATOR_FOLDER_PRACTICE_QUESTION/Question_part2.py
@@ -1,5 +1,39 @@
-#What is the output of the addition operation 10 + 5?
+#Question1
+# What is the output of the addition operation 10 + 5?
 a=10
 b=5
 result=a+b
 print(result)
+#Answer=The code uses the addition operator (+) to add the values of a and b together, outputting the final sum of 15.
+
+
+#Question2
+# Which operator calculates the remainder, and what is the specific result of 10 % 
+c=10
+d=5
+print(c%d)
+#Answer=The modulus operator (%) is used to calculate the remainder, and the specific result of 10 % 5 is 0.
+
+
+# Question3
+# How does the floor division operator (//) evaluate the expression 10 // 3?
+e=10
+f=3
+print(e//f)
+# Answer=The floor division operator (//) evaluates the expression by returning only the integer quotient, so the specific result of 10 // 3 is 3.
+
+
+# Question4
+# What is the output of the exponentiation calculation 2 ** 3?
+m=2
+n=3
+print(m**n)
+# Answer=The output of the exponentiation calculation 2 ** 3 is 8.  
+
+
+# Question5
+# If variables are set as g=10 and h=5, what is the exact float output of the division operation print(g/h)
+g=10
+h=5
+print(g/h)
+# Answer=The exact float output of the division operation print(g/h) is 2.0.
