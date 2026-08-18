0eeab74a8d6f269fd7c73e4b5bd36f2fa8a36bea
Subhradip Bhunia <smya9971@gmail.com>
2026-08-18 21:52:18 +0530

    add new file

--- Introduction_To_Python/OPERATORS/OPERATOR_FOLDER_PRACTICE_QUESTION/Question_part2.py
+++ Introduction_To_Python/OPERATORS/OPERATOR_FOLDER_PRACTICE_QUESTION/Question_part2.py
@@ -37,3 +37,39 @@ g=10
 h=5
 print(g/h)
 # Answer=The exact float output of the division operation print(g/h) is 2.0.
+
+
+# Question6
+# Does the expression 5 == 5 evaluate to True or False?
+i=5
+j=5
+print(i==j)
+# Answer=The == (Equal to) operator checks if two values are exactly the same. Since the number 5 is equal to itself, the condition is met and it outputs True.  
+
+
+# Question7
+# If you use the == operator to compare an integer and a string that hold the exact same value, what will the output be?
+k="5"
+l=5
+print(k==l)
+# Answer=Even if the values appear the same, comparing an integer and a string results in False because the == operator recognizes that they are different data types.
+
+
+# Question8 
+# Given the variables A=10 and B=20, what does the inequality check print(A!=B) output?  
+A=10
+B=20
+print(A!=B)
+# Answer=The != (Not equal to) operator checks if two variables are different from each other. Since 10 is not equal to 20, the condition is satisfied, resulting in True.  
+
+
+# Questionn9 
+# What does the >= operator specifically check for when comparing a first and second variable?
+# Answer=It evaluates whether the first value is larger than the second value, or if the two variables are exactly the same. If either of these conditions is met, it outputs True.
+
+
+# Question10
+# If age=20, does the boolean condition age>=18 return True or False?
+age=20
+print(age>18)
+# Answer=The >= operator checks if the value of age is greater than or equal to 18. Since 20 is strictly greater than 18, the condition is satisfied, resulting in True.
