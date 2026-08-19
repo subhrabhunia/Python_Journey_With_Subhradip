d902e9b977d36142d44377a5990552d89211380d
Subhradip Bhunia <smya9971@gmail.com>
2026-08-19 21:18:10 +0530

    add new file

---
+++ Introduction_To_Python/STATEMENT/01-ifstatement.py
@@ -0,0 +1,38 @@
+# If statement use to check the condition of a code,Its can give the results True or False,If the result is True then give a special statement and if condilions output is False then it out of the code.
+# SYNTAX OF IF STATEMENT
+#if condition:
+    #  Code Statements
+
+
+a=10
+if a==10:
+    print("Matched")
+
+print("End")
+
+b="20"   #the value of b is string
+if b==20:
+    print("Matched")
+
+print("End")
+
+c=5
+if a>c :
+    print("A is greater")
+print("End")
+
+
+# Real-Life Example
+User_Age= float(input("Enter Your Age:"))
+if User_Age>=18:
+    print("You are Eligable for Vote")
+if User_Age<18:
+    print("You are Not Eligablbe for vote")
+
+
+Number= float(input("Enter Your Number:"))
+if Number>=0:
+    print("The Number is Positive")
+if Number<0:
+    print("The Number is Negative")
+
