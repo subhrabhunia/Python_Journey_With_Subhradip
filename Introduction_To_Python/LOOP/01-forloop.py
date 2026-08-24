10bba8c300ea8ddb35c6e880a87163b065c4a588
Subhradip Bhunia <smya9971@gmail.com>
2026-08-25 00:17:12 +0530

    add new file

--- Introduction_To_Python/LOOP/01-forloop.py
+++ Introduction_To_Python/LOOP/01-forloop.py
@@ -9,3 +9,29 @@
 # 4.dictionary
 # 5.set
 # 6.range of numbers
+
+
+#Example
+x=["HTML","CSS","JS","Python","MySQL"]
+for subject in x:
+    print(subject)
+
+#List example
+fruits=["Apple","Guava","Orange",'Banana']
+for fruit in fruits:
+    print(fruit)
+
+#Tuple Example
+Names=("Subhra","Soumya","Akash","Biswajit")
+for name in Names:
+    print(name)
+
+#Set Example
+Girls={"Riya","Sita","Mou","Mita"}
+for girl in Girls:
+    print(girl)
+
+#String Example
+subject="Python"
+for letter in subject:
+    print(letter)
