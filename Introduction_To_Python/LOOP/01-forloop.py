0415ba4f1028da887c7b92c9765ba0ac65f6fc70
Subhradip Bhunia <smya9971@gmail.com>
2026-08-24 23:57:54 +0530

    add new file

---
+++ Introduction_To_Python/LOOP/01-forloop.py
@@ -0,0 +1,11 @@
+# SYNTAX OF FOR LOOP:
+# for variable in sequence:
+#     #Code to execute
+
+# which we use for loop
+# 1.list
+# 2.tuple
+# 3.String
+# 4.dictionary
+# 5.set
+# 6.range of numbers
