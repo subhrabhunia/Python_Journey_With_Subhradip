895997b12064d38bbac83d5ead44dbe77d243566
Subhradip Bhunia <smya9971@gmail.com>
2026-08-25 23:19:26 +0530

    add new file

--- Introduction_To_Python/LOOP/02-whileloop.py
+++ Introduction_To_Python/LOOP/02-whileloop.py
@@ -52,3 +52,46 @@ counts=1
 while counts<=20:
     print(counts)
     counts=counts+2
+
+#In reverse
+count_reverse=10
+while count_reverse>=1:
+    print(count_reverse)
+    count_reverse=count_reverse-1
+
+#multiplication table
+num=8
+i=1
+while i<=10:
+    print(f"{num} X {i} = {num*i}")
+    i=i+1
+
+#While else
+counting=1
+while counting<=6:
+    print(counting)
+    counting=counting+1
+else:
+    print("Loop FINISHED")
+
+#In List
+fruits=["Apple","Banana","Orange"]
+j=0
+while j< len(fruits):
+    print(fruits[j])
+    j+=1
+
+
+#get user password
+password=""
+while password !="python123":
+    password=input("Entetr password:")
+print("Password match")
+
+#Sum of input value
+total=0
+number=1
+while number!=0:
+    number=int(input("enter a number (0 to stop)"))
+    total+=0
+print("Total=",total)
