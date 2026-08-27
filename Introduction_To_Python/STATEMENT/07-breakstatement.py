08b075a595f8dc7d5c1b8c6e41918418a94a1f67
Subhradip Bhunia <smya9971@gmail.com>
2026-08-28 00:10:44 +0530

    add new file

--- Introduction_To_Python/STATEMENT/07-breakstatement.py
+++ Introduction_To_Python/STATEMENT/07-breakstatement.py
@@ -1,22 +1,22 @@
-# #Example
-# for i in range(1,11):
-#     if i == 5:
-#         break
-#     print(i)
-# print("Loop Finished")
+#Example
+for i in range(1,11):
+    if i == 5:
+        break
+    print(i)
+print("Loop Finished")
 
-# #IN list
-# fruits = ["Apple", "Banana", "Orange", "Mango"]
+#IN list
+fruits = ["Apple", "Banana", "Orange", "Mango"]
 
-# search = "Banana"
+search = "Banana"
 
-# for fruit in fruits:
-#     if fruit == search:
-#         print("Fruit Found")
-#         break
-# else:
-#     print("Not Found")  #This working when the item not found in the list
-# print("Search Completed")
+for fruit in fruits:
+    if fruit == search:
+        print("Fruit Found")
+        break
+else:
+    print("Not Found")  #This working when the item not found in the list
+print("Search Completed")
 
 #pin Mannagement
 correct_pin = "1234"
