96ec623bceb3d61ff4c7aab01d8fe89ffd71bf7b
Subhradip Bhunia <smya9971@gmail.com>
2026-08-28 01:59:37 +0530

    add new file

---
+++ Introduction_To_Python/STATEMENT/09-passstatement.py
@@ -0,0 +1,21 @@
+# CONTROL STATEMENTS:-
+# There are three statements are used to control the flow of a program.
+# Break:-Stops the entire loop immediately.
+# Continue:-Skips the current iteration and continue with next one.
+# Pass:- Do nothing.
+
+# Example
+age=18
+if age>=18:
+    pass
+print("WORK IN PROGRESS")
+
+#Example 2
+numbers = [10, 20, 30]
+
+for num in numbers:
+
+    if num == 20:
+        pass
+
+print(num)
