be18bdc67aa3f13e224b7c18f17795280506aee8
Subhradip Bhunia <smya9971@gmail.com>
2026-08-28 20:51:45 +0530

    add new file

--- Introduction_To_Python/LOOP/LOOP_PATTERN/02-Pattern2.py
+++ Introduction_To_Python/LOOP/LOOP_PATTERN/02-Pattern2.py
@@ -18,3 +18,20 @@ for c in range(5,0,-1):
         print(c, end=" ")
 
     print()
+
+#Rverse(Inverted) Right angle pattern:4
+for C in range(5,0,-1):
+    for D in range(C,0,-1):
+        print(D, end=" ")
+
+    print()
+
+#Rverse(Inverted) Right angle pattern:5
+value=1
+for e in range(5,0,-1):
+    for f in range(e):
+        print(value, end=" ")
+        
+
+    print()
+    value+=1
