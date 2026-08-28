5ba7c963810c5b8b3aaeeaab4e092d748539de9f
Subhradip Bhunia <smya9971@gmail.com>
2026-08-28 22:04:10 +0530

    add new file

--- Introduction_To_Python/LOOP/LOOP_PATTERN/03-Pattern3.py
+++ Introduction_To_Python/LOOP/LOOP_PATTERN/03-Pattern3.py
@@ -5,10 +5,24 @@ for a in range(1, rows + 1):
 
     # For spaces
     for c in range(rows - a):
-        print(" ", end="")
+        print(" ", end=" ")
 
     # For stars
     for b in range(a):
         print("*", end=" ")
+
+    print()
+
+#Left Triangle Pattern:2
+row = 5
+for A in range(1, row + 1):
+
+    # For spaces
+    for C in range(row - A):
+        print(" ", end=" ")
+
+    # For number
+    for B in range(1,A+1):
+        print(B, end=" ")
 
     print()
