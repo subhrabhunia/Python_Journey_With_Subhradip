c5ba82ac93956b40eb4e1d2f6e2aaa2caa620899
Subhradip Bhunia <smya9971@gmail.com>
2026-08-29 15:59:14 +0530

    add new file

---
+++ Introduction_To_Python/FUNCTIONS/01-Function.py
@@ -0,0 +1,28 @@
+# def greet():  --->Function defination
+#     print("Welcome to python!")
+#     print("Learn python Functions")
+# greet()  --->calling Function
+# ------
+# ------
+# greet
+
+# Advantages of Functions:
+# 1.Reusable code.
+# 2.Less duplication.
+# 3.Easier debugging.
+# 4.Better organization.
+# 5.Easier testing.
+# 6.Modular programming.
+# 7.Improves readability.
+
+#Example
+def greet():
+    print("Welcome to my github profile")
+    print("My Name is Subhradip")
+greet()
+greet()
+print("---------------------")
+greet()
+greet()
+print("---------------------")
+greet()
