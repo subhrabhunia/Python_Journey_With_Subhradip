9daf5e50915242c22d27b7cfbf32addc2508a84a
Subhradip Bhunia <smya9971@gmail.com>
2026-08-31 01:24:24 +0530

    add new file

--- Introduction_To_Python/FUNCTIONS/05-Keywordvariable-lengtharguments.py
+++ Introduction_To_Python/FUNCTIONS/05-Keywordvariable-lengtharguments.py
@@ -6,3 +6,16 @@ def student(**keywordargs):
     print(keywordargs)
 student(name="Subhraip",age=20,city="Kolkata")
 
+#if print one by boone value
+def students(**keyarg):
+    print(keyarg['name'])     
+    print(keyarg['age'])
+    print(keyarg['city'])
+students(name="Subhraip",age=20,city="Kolkata")
+
+
+#using loop
+def students_Name(**keyargs):
+    for key, value in keyargs.items():
+        print(key, ":", value)
+students_Name(name="Subhraip", age=20, city="Kolkata")
