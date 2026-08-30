5ff4d24e1c1ef4a880802934cf49c1f7d1ab77a3
Subhradip Bhunia <smya9971@gmail.com>
2026-08-31 00:48:10 +0530

    add new file

---
+++ Introduction_To_Python/FUNCTIONS/04-Variablelengthsarguments.py
@@ -0,0 +1,23 @@
+# def sum(*args):
+#     Code Statement.  *Star meanslength arguments and args or any other name work as Tuple.Accept any  number of positional arguments.
+
+#example
+def num (*args):
+    print(args)
+
+num(10)
+num(10,20)
+num(10,20,30)
+
+#print with index number
+def numbers(*test):    
+    print(test[0])
+    print(test[2])   
+
+numbers(10,20,30)
+
+#print with loop
+def number(*tests):    
+    for numb in tests:
+        print(numb)
+number(10,20,30)
