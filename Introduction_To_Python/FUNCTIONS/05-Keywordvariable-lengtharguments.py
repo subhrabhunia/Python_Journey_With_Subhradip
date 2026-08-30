e2554ba8023f6455cbf86cf301dcc4dcddf8b1f1
Subhradip Bhunia <smya9971@gmail.com>
2026-08-31 01:18:31 +0530

    add new file

---
+++ Introduction_To_Python/FUNCTIONS/05-Keywordvariable-lengtharguments.py
@@ -0,0 +1,8 @@
+#def demo(**args):
+#    code statement.    Its works as a dictionary.its accept any number of keyword arguments.** means keyword arguments
+
+# Basic example
+def student(**keywordargs):
+    print(keywordargs)
+student(name="Subhraip",age=20,city="Kolkata")
+
