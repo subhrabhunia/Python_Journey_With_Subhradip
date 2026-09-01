73df6d330ea0f92ef0db9905fc1b00fe61cd6963
Subhradip Bhunia <smya9971@gmail.com>
2026-09-02 00:23:14 +0530

    add new file

---
+++ Introduction_To_Python/FUNCTIONS/08-Lamdbafunction.py
@@ -0,0 +1,16 @@
+# This is a one line function which is defined using the keyword lambda. It can take any number of arguments, but can only have one expression. The expression is evaluated and returned. Lambda functions can be used wherever function objects are required. They are syntactically restricted to a single expression. You can use lambda functions when you require a nameless function for a short period of time.
+# SYNTAX:-
+# sum=lambda a,b:a+b
+# print(sum(5,6)) # Output: 11
+# Features of Lambda Functions:
+# 1.has no function name (i.e. it is an anonymous function)
+# 2.can take any number of arguments
+# 3.is written in one line 
+# 4.can contain only a single expression 
+# 5.Automatically returns the value of the expression
+
+
+#Basic Example
+sum=lambda a,b:a+b
+print (sum(10,20)) 
+print (sum(13,20)) 
