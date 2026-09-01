a0ace6dc14f3bb91a159d14a71e887a3f33875e0
Subhradip Bhunia <smya9971@gmail.com>
2026-09-02 02:17:26 +0530

    add new file

--- Introduction_To_Python/FUNCTIONS/09-Recursivefunction.py
+++ Introduction_To_Python/FUNCTIONS/09-Recursivefunction.py
@@ -13,3 +13,20 @@ def print_numbers(n):
     print(n)
     print_numbers(n+1)  # Recursive call with modified parameter
 print_numbers(1)  # Start the recursion with initial value 1
+
+def countdown(n):
+    if n == 0:  # Base case to stop recursion
+        print("Countdown complete!")
+        return
+
+    print(n)
+    countdown(n-1)  # Recursive call with modified parameter
+print("Countdown from 5:")
+countdown(5)  # Start the countdown from 5
+
+#factorial of a number using recursion of 5
+def factorial(n):
+    if n==1:
+        return 1
+    return n * factorial(n-1)  # Recursive call with modified parameter
+print("Factorial of 5 is:", factorial(5))  # Output: Factorial of 5 is: 120
