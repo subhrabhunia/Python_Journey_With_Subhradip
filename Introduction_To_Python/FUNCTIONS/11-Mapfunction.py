4a94af8659e1e48ccbf1848150698a4c7fdfb7c3
Subhradip Bhunia <smya9971@gmail.com>
2026-09-04 22:24:34 +0530

    add new file

--- Introduction_To_Python/FUNCTIONS/11-Mapfunction.py
+++ Introduction_To_Python/FUNCTIONS/11-Mapfunction.py
@@ -4,3 +4,9 @@
 # The map() function applies a given function to all items in an iterable (like a list)
 # map() function takes two arguments: a function and an iterable (like a list, tuple, etc.) and returns an iterator that applies the function to each item of the iterable.Function can be a built-in function or a user-defined function.External functions and Lambda functions can also be used with map() function.
 
+#Example Squaring numbers in a list using map() function
+numbers = [1, 2, 3, 4, 5]
+def square(x):
+    return x*x
+result = map(square, numbers)
+print(list(result))  # Output: [1, 4, 9, 16, 25]
