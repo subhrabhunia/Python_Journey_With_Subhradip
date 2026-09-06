9e6be53d565db97a783b94024930aa2befb7e00c
Subhradip Bhunia <smya9971@gmail.com>
2026-09-06 23:40:32 +0530

    add new file

---
+++ Introduction_To_Python/FUNCTIONS/12-Filterfunction.py
@@ -0,0 +1,12 @@
+# SYNTAX:-
+# The filter() function returns only the elements that satisfy a certain condition from an iterable (like a list, tuple, etc.). It takes two arguments: a function and an iterable. The function should return either True or False for each element in the iterable. The filter() function returns an iterator that contains only the elements for which the function returned True.
+# filter(function, iterable)
+
+# | Feature                       | `map()` Function                  | `filter()` Function              |
+# | ----------------------------- | --------------------------------- | -------------------------------- |
+# | **Purpose**                   | Modifies/transforms every element | Selects some elements            |
+# | **Returns**                   | Transformed values                | Matching values                  |
+# | **Function should return**    | Any value                         | `True` or `False`                |
+# | **Number of output elements** | Same as input                     | Same or fewer than input         |
+# | **Common use cases**          | Convert, calculate, format        | Search, validate, remove, filter |
+
