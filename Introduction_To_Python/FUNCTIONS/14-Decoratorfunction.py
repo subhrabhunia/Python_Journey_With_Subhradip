06077c2df2da9e477657f801c2c0b5f443e2c416
Subhradip Bhunia <smya9971@gmail.com>
2026-09-08 01:47:19 +0530

    add new file

---
+++ Introduction_To_Python/FUNCTIONS/14-Decoratorfunction.py
@@ -0,0 +1,50 @@
+# Input Function
+#       |
+#       |
+#       v
+# def decorator(function):
+#       |
+#       |  Extra code / behavior
+#       |
+#       v
+# Output Function
+# (with Extra Behavior)
+
+# SYNTAX:-
+# def decorator(func):
+#     def wrapper():
+#         print("Before")
+#         func()
+#         print("After")
+    
+#     return wrapper
+
+
+# @decorator
+# def hello():
+#     print("Hello World")
+
+
+# hello()
+
+# Flow:-
+# @decorator
+#     ↓
+# hello function
+#     ↓
+# decorator(hello)
+#     ↓
+# wrapper function
+#     ↓
+# hello()
+#     ↓
+# Before
+# Hello World
+# After
+
+# Real Use Case of decorator:
+# 1.Log messages
+# 2.Check Login
+# 3.Check Permission
+# 4.Calculate execution time
+
