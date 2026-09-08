0d8aeb696cb836f58df97218daa50bcafc361589
Subhradip Bhunia <smya9971@gmail.com>
2026-09-09 00:06:25 +0530

    add new file

---
+++ Introduction_To_Python/FUNCTIONS/15-Generatorfunctions.py
@@ -0,0 +1,9 @@
+# SYNTAX:
+# def function_name:
+#         yield"FIRST"   ------>Pause
+#         yield"SECOND"
+#         yield"THIRD"
+# g=function_name()
+# print(next(g))----->print FIRST
+# print(next(g))----->print SECOND
+# print(next(g))----->print THIRD
