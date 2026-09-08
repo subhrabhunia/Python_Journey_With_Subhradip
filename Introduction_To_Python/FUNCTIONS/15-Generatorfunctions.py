ad09d96c48af722b607bbd0df88fbbe0097f7e62
Subhradip Bhunia <smya9971@gmail.com>
2026-09-09 00:10:11 +0530

    add new file

--- Introduction_To_Python/FUNCTIONS/15-Generatorfunctions.py
+++ Introduction_To_Python/FUNCTIONS/15-Generatorfunctions.py
@@ -1,9 +1,19 @@
 # SYNTAX:
 # def function_name:
-#         yield"FIRST"   ------>Pause
+#         yield"FIRST"   ------>yield means Pause
 #         yield"SECOND"
 #         yield"THIRD"
 # g=function_name()
 # print(next(g))----->print FIRST
 # print(next(g))----->print SECOND
 # print(next(g))----->print THIRD
+
+#Basic Example
+def numbers():
+    yield 1
+    yield 2
+    yield 3
+g=numbers()
+print(next(g))
+print(next(g))
+print(next(g))
