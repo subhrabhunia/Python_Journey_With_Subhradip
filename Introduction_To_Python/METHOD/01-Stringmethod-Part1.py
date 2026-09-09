93fb29bf1b354f8a0318d4cfe550a8f49587abd4
Subhradip Bhunia <smya9971@gmail.com>
2026-09-10 00:47:57 +0530

    add new file

---
+++ Introduction_To_Python/METHOD/01-Stringmethod-Part1.py
@@ -0,0 +1,27 @@
+a = "Hello, World!"
+
+print(a)
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+print(a[6])
+print(a[7])
+print(a[8])
+print(a[9])
+print(a[10])
+print(a[11])
+print(a[12])
+
+# String Slicing
+print(a[:5])
+print(a[-1:-5:-1])
+
+#for loop
+for x in a:
+    print(x)
+
+#length method
+print(len(a))
