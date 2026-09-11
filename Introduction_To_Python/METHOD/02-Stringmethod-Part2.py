f877d27e5bf3a3c12a2199b7a23bbd9cf850d09c
Subhradip Bhunia <smya9971@gmail.com>
2026-09-12 01:26:31 +0530

    add new file

--- Introduction_To_Python/METHOD/02-Stringmethod-Part2.py
+++ Introduction_To_Python/METHOD/02-Stringmethod-Part2.py
@@ -7,3 +7,18 @@ print("python@123".isalnum())
 print("Programming".isalpha())  #Check only alphabet
 print("Programming ".isalpha())
 print("Python Programming".isalpha())
+
+#is digit method
+print("123".isdigit())  #Check only digit
+print("123 ".isdigit())
+print("123.45".isdigit())
+
+#islower method
+print("Subhradip".islower())   #its chechk all alphabet is lower case
+print("subhradip".islower())
+print("subhradip123".islower())
+print("SubhraDip".islower())
+
+#isupper method
+print("Subhradip".isupper()) #its check is all alphabet in upper case
+print("SUBHRADIP".isupper())
