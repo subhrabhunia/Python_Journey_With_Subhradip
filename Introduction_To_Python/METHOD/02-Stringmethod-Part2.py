31c9c1d3daaf24a22ce6d1b1c5722abdc43e2009
Subhradip Bhunia <smya9971@gmail.com>
2026-09-12 01:19:45 +0530

    add new file

---
+++ Introduction_To_Python/METHOD/02-Stringmethod-Part2.py
@@ -0,0 +1,9 @@
+#isalnum method
+print("python123".isalnum())   #Check the alphabet and nuemeric value
+print("python 123".isalnum())
+print("python@123".isalnum())
+
+#isalpha method
+print("Programming".isalpha())  #Check only alphabet
+print("Programming ".isalpha())
+print("Python Programming".isalpha())
