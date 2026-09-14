b7961a4718729f8f680431a67f522067a0258441
Subhradip Bhunia <smya9971@gmail.com>
2026-09-15 01:46:51 +0530

    add new file

---
+++ Introduction_To_Python/METHOD/05-List-Comprehensions-Part2.py
@@ -0,0 +1,9 @@
+#Set Comperehension
+numbers=[1,2,2,3,3,4,5]
+unique={num for num in numbers}
+print(unique)
+
+#Square Value of Set
+Numbers=[1,2,3,4,5]
+Squares={n**2 for n in Numbers}
+print(Squares)
