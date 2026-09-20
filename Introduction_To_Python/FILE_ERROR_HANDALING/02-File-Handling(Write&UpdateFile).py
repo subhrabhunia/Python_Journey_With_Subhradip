85bd8f7cbb1ecb3719ef5be3707ddee092bf3866
Subhradip Bhunia <smya9971@gmail.com>
2026-09-20 23:32:10 +0530

    add new file

--- Introduction_To_Python/FILE_ERROR_HANDALING/02-File-Handling(Write&UpdateFile).py
+++ Introduction_To_Python/FILE_ERROR_HANDALING/02-File-Handling(Write&UpdateFile).py
@@ -1,8 +1,16 @@
+#Create and Write File
 file = open("D:\\Demo_Python\\Python_Journey_With_Subhradip\\Introduction_To_Python\\FILE_ERROR_HANDALING\\File.txt", "w")
-
 file.write("Hello Python Developr\n")
 file.write("Subhradip\n")
 file.write("Soumya\n")
 file.write("Ram\n")
+
+#Writee lines
+names=[
+    "Jodhu\n",
+    "Modhu\n",
+    "Shayam\n"
+]
+file.writelines(names)
 
 file.close()
