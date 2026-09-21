c70a85ee4e44ffe2ecf24d85e69501ee53145007
Subhradip Bhunia <smya9971@gmail.com>
2026-09-22 01:24:30 +0530

    add new file

--- Introduction_To_Python/FILE_ERROR_HANDALING/04-File-handiling(With-Statement).py
+++ Introduction_To_Python/FILE_ERROR_HANDALING/04-File-handiling(With-Statement).py
@@ -9,3 +9,25 @@ with open("D:\\Demo_Python\\Python_Journey_With_Subhradip\\Introduction_To_Pytho
     for line in file1:
         count += 1
 print("Total Lines:", count)
+
+#Write Example
+with open("D:\\Demo_Python\\Python_Journey_With_Subhradip\\Introduction_To_Python\\FILE_ERROR_HANDALING\\File.txt", "w") as file:
+    file.write("RAM\n")
+    file.write("Shayam\n")
+    file.write("Jodhu")
+print("File Written Successfully.")
+
+#Append File-Notes as a application
+student=input("Enter a student name:")
+with open("D:\\Demo_Python\\Python_Journey_With_Subhradip\\Introduction_To_Python\\FILE_ERROR_HANDALING\\File.txt", "a") as file:
+    file.write(student+ "\n")
+print("Saved name Successfully")
+
+#Copy method
+with open("D:\\Demo_Python\\Python_Journey_With_Subhradip\\Introduction_To_Python\\FILE_ERROR_HANDALING\\File.txt", "r") as source:
+    content=source.read
+
+with open("D:\\Demo_Python\\Python_Journey_With_Subhradip\\Introduction_To_Python\\FILE_ERROR_HANDALING\\newfile.txt", "w") as destination:
+    destination.write(content)
+print("File Copied")
+
